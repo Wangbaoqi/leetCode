c97d9fff3a745dee29ff1118fecd2e1de727c026
Nate Wang <baoqiwang@outlook.com>
2021-10-29 14:01:41 +0800

    数组 双指针

--- 数组/颜色分类/75.颜色分类.js
+++ 数组/颜色分类/75.颜色分类.js
@@ -31,7 +31,7 @@ const sortColors_count = function(nums) {
   let blue_count = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if(nums[i] === 0) red_count++;    
+    if(nums[i] === 0) red_count++;
     if(nums[i] === 1) white_count++;    
     if(nums[i] === 2) blue_count++;    
   }
@@ -49,13 +49,91 @@ const sortColors_count = function(nums) {
 };
 
 
-// * 2. single pointer
+// * 2. single pointer 
+
+const sortColors_singlePointer = (nums) => {
+
+  let p = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] == 0) {
+      let tmp = nums[i];
+      nums[i] = nums[p];
+      nums[p]  = tmp;
+      p++
+    }
+  }
+
+  for (let i = p; i < nums.length; i++) {
+    if(nums[i] == 1) {
+      let tmp = nums[i];
+      nums[i] = nums[p];
+      nums[p] = tmp;
+      p++
+    }    
+  }
+}
 
 
+// * 3. double pointer - Fast slow pointer
+
+// need two pointer p0 p1
+// p0 < p1 exchange cur i and p1, make sure that 1 will not moved 
+// keyword point is that when i == 0
+
+const sortColors_doublePointer = (nums) => {
+  let p0 = 0, p1 = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] == 1) {
+      let tmp = nums[i];
+      nums[i] = nums[p1];
+      nums[p1] = tmp;
+      p1++
+    }else if(nums[i] == 0) {
+      let tmp = nums[i];
+      nums[i] = nums[p0];
+      nums[p0] = tmp;
+      if(p0 < p1) {
+        let tmp = nums[i];
+        nums[i] = nums[p1]
+        nums[p1] = tmp
+      }
+      p0++;
+      p1++;
+    }
+  }
+}
 
 
-// * 3. double pointer
+// * 4. double pointer - Collision pointer
 
+// need two pointer p0 = 0 p2 = len - 1
+// traverse arr 
+// 1. while nums[i] == 2 exchange i and p2 && p2--
+// 2. if nums[i] == 0 exchange and i and p0 
+
+const sortColors_doublePointer1 = (nums) => {
+
+  let p0 = 0, p2 = nums.length - 1;
+
+  for (let i = 0; i < p2; i++) {
+    
+    while(i < p2 && nums[i] == 2) {
+      let tmp = nums[i];
+      nums[i] = nums[p2];
+      nums[p2] = tmp;
+      p2--;
+    }
+
+    if(nums[i] == 0) {
+      let tmp = nums[i];
+      nums[i] = nums[p0];
+      nums[p0] = tmp;
+      p0++
+    }
+  }
+}
 
 
 

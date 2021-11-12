1161b608c3998b9aeb6d19409037cd830fcd5db6
Nate Wang <baoqiwang@outlook.com>
2021-11-12 15:51:18 +0800

    「四数之和」「接雨水」

--- 数组/三数之和/15.三数之和 4.js
+++ 数组/三数之和/15.三数之和 4.js
@@ -39,10 +39,11 @@ const threeSum = nums => {
 
       if(sum == 0) {
         res.push([nums[i], nums[j], nums[z]])
+        // point compare fourSum 
+        while(j < z && nums[j] == nums[j + 1]) j++;
         j++;
-        z--;
-        while(j < z && nums[j] == nums[j - 1]) j++;
         while(j < z && nums[z] == nums[z - 1]) z--;
+        z--;
       }else if(sum > 0){
         j++
       }else {

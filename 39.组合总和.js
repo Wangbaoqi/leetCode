57be060f5013d3f43cbf29ab82b4b71a35aecde8
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-03-15 13:55:19 +0800

    「每日一题」回溯算法

---
+++ 39.组合总和.js
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=39 lang=javascript
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+
+};
+// @lc code=end
+
+
+
+const combinationSum1 = (candidates, target) => {
+
+  const ret = [], ans = [];
+  const len = candidates.length;
+
+  const ansSum = ans => {
+    return ans.reduce((p, c) => p + c, 0)
+  }
+
+  const backTracking = (len, target, i) => {
+
+    const sum = ansSum(ans);
+    // stop condition
+    if (sum === target || sum > target) {
+      (sum === target) && ret.push([...ans]);
+      return
+    }
+
+    for (let j = i; j < len; j++) {
+      ans.push(candidates[j]);
+      backTracking(len, target, j);
+      ans.pop()
+    }
+  }
+
+  backTracking(len, target, 0);
+  return ret;
+}
+
+const combinationSum2 = (candidates, target) => {
+
+  const ret = [], ans = [];
+  const len = candidates.length;
+
+
+  const backTracking = (s, target, i) => {
+    
+    // stop condition
+    if (s === target || s > target) {
+      (s === target) && ret.push([...ans]);
+      return
+    }
+
+    for (let j = i; j < len; j++) {
+      ans.push(candidates[j]);
+      backTracking(s + candidates[j], target, j);
+      ans.pop()
+    }
+  }
+
+  backTracking(0, target, 0);
+  return ret;
+}
+
+const res = combinationSum2([2,3,5], 8)
+
+console.log(res);
--- 77.组合.js
+++ 77.组合.js
@@ -18,27 +18,31 @@ var combine = function(n, k) {
 
 const combine_1 = (n, k) => {
 
-
-
-  const backTracking = () => {
-
-    
-  }
-
-  // 终止条件
-  if () {
-    
-  }
-
-  for (let i = 1; i <= n; i++) {
-
-    for (let j = i + 1; j <= n; j++) {
-
-
-    }
-
-  }
-
   const res = [], ans = [];
 
+  const backTracking = (n, k, i) => {
+
+      // 终止条件 ans.length == k
+    if (ans.length == k) {
+      res.push([...ans])
+      return;
+    }
+
+    const p = n - ans.length + 1
+    for (let j = i; j <= p; j++) {
+      ans.push(j); // 处理节点
+      backTracking(n, k, j + 1); // 递归遍历
+      ans.pop() // 回溯操作
+    }
+  }
+
+  backTracking(n, k, 1)
+
+  return res;
+
 }
+
+
+const ret = combine_1(4, 2);
+
+console.log(ret);

60b3f53e1c75bc0b3b916bd558b67d4170ddc86b
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-03-19 23:12:58 +0800

    「每日一题」回溯

--- 491.递增子序列.js
+++ 491.递增子序列.js
@@ -23,31 +23,24 @@ const findSubsequences_a = nums => {
 
     if (ans.length > 1) {
       ret.push([...ans])
-      
     }
     // 在横向遍历的时候 遇到重复的则直接 continue 
     const set = new Set();
-    const used = Array(200).fill(0);
     for (let j = i; j < nums.length; j++) {
 
       if (ans[ans.length - 1] > nums[j] || set.has(nums[j])) {
         continue;
       }
-      // if (ans[ans.length - 1] > nums[j] || used[nums[j] + 100] == 1) {
-      //   continue;
-      // }
       set.add(nums[j]);
-      used[nums[j] + 100] = 1
       ans.push(nums[j])
       backTracking(nums, j + 1);
       ans.pop()
     }
   }
-
   backTracking(nums, 0)
   return ret;
 }
 
-const test = [4, 7, 7, ];
+const test = [4, 6, 7, 7];
 const res = findSubsequences_a(test);
 console.log(res);

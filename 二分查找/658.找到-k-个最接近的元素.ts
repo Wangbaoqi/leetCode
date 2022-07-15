2e6677728fbb09816da9ebeb8be6ec962a1ab117
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-07-15 11:31:42 +0800

    feat: add post algo

--- 二分查找/658.找到-k-个最接近的元素.ts
+++ 二分查找/658.找到-k-个最接近的元素.ts
@@ -30,3 +30,26 @@ function findClosestElements(arr: number[], k: number, x: number): number[] {
 };
 // @lc code=end
 
+const findClosestElements1 = (arr: number[], k: number, x: number): number[] => {
+
+  let n = arr.length;
+  let l = 0, r = n - 1, res: number[] = [];
+
+  let rem = n - k;
+
+  while (rem > 0) {
+    
+    if (x - arr[l] <= arr[r] - x) {
+      r--
+    }else {
+      l++
+    }
+    rem--
+  }
+
+  for (let i = l; i < l + k; i++) {
+    res.push(arr[i])
+  }
+
+  return res;
+}

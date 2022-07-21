ae1d86644059ed7720b2e33dce92aef9da95a7bf
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-07-21 18:34:15 +0800

    feat: add algo day

---
+++ 4.寻找两个正序数组的中位数.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=typescript
+ *
+ * [4] 寻找两个正序数组的中位数
+ */
+
+// @lc code=start
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+
+};
+// @lc code=end
+
--- 二分查找/154.寻找旋转排序数组中的最小值-ii.ts
+++ 二分查找/154.寻找旋转排序数组中的最小值-ii.ts
@@ -1,7 +1,7 @@
 /*
- * @lc app=leetcode.cn id=153 lang=typescript
+ * @lc app=leetcode.cn id=154 lang=typescript
  *
- * [153] 寻找旋转排序数组中的最小值
+ * [154] 寻找旋转排序数组中的最小值 II
  */
 
 // @lc code=start
@@ -9,16 +9,18 @@ function findMin(nums: number[]): number {
   let n = nums.length;
 
   let l = 0, r = n - 1;
+
   while (l < r) {
     let m = l + ((r - l) >> 1);
-
     if (nums[m] < nums[r]) {
       r = m
-    } else {
+    } else if (nums[m] > nums[r]) {
       l = m + 1
+    } else {
+      r -= 1
     }
   }
-  return nums[r]
+  return nums[l]
 };
 // @lc code=end
 

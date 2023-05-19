4da690ef8651a6a50cc263ecb2c9a5bce809b1cc
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-05-19 11:23:57 +0800

    feat: add unit test getKthFromEnd

--- src/LinkList/deleteMiddleNode/deleteMiddleNode.test.ts
+++ src/LinkList/deleteMiddleNode/deleteMiddleNode.test.ts
@@ -1,20 +1,22 @@
 
-import { ListNode, getKthFromEnd } from "./返回倒数第 k 个节点";
+import { ListNode, deleteNode } from "./删除中间节点";
 
 
-describe('test getKthFromEnd', () => { 
+describe('test deleteNode', () => { 
 
-  it('test_normal_kthFromEnd', () => { 
+  it('test_normal_deleteNode', () => { 
 
     const head = new ListNode(1);
     const node = new ListNode(2);
     const node1 = new ListNode(3);
+
     head.next = node;
     node.next = node1;
 
     const expected = head;
     expected.next = node;
 
-    expect(getKthFromEnd(head, 3)).toEqual(head);
+    // expect(deleteNode(head)).toEqual(head);
+
   })
 })
--- src/LinkList/deleteMiddleNode/删除中间节点.ts
+++ src/LinkList/deleteMiddleNode/删除中间节点.ts
@@ -16,11 +16,20 @@
  *     }
  * }
  */
+
+export class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
+}
+
 /**
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
-
 export function deleteNode(node: ListNode): ListNode | null {
   const tmp = node.next!;
   node.val = tmp.val;

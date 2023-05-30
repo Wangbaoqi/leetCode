b61c73b4e416930b507de3119f778b19e71d38dc
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-05-30 16:59:19 +0800

    feat: update linkList overview

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wangbaoqi', // Usually your GitHub org/user name.
+  projectName: 'leetCode', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Wangbaoqi/leetCode/blob/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Wangbaoqi/leetCode/blob/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),

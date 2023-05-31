87239113f84ef3edd5aa1c86c4b0168c8ab39503
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-05-31 16:35:39 +0800

    update codeBlock format

--- docs/algo-linkList/leetCode/designHashSet/designHashSet.test.ts
+++ docs/algo-linkList/leetCode/designHashSet/designHashSet.test.ts
@@ -1,18 +1,11 @@
-import { ListNode, MyHashSet } from '.';
+import { MyHashSet } from '.';
 
 describe('test getDecimalValue', () => { 
 
   it('test_normal_decimal_value', () => {
-    const head = new ListNode(
-      1,
-      new ListNode(
-        0,
-        new ListNode(
-          1
-        )
-      )
-    );
+    
 
   });
   
 })
+

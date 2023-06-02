1899bfa3240284e77b3ded0ff79861ae05a1d7d0
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-06-02 18:15:16 +0800

    feat: add swapNodes for linklist

---
+++ docs/algo-linkList/leetCode/swapNodeInPair/index.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=typescript
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+}
+
+export function swapPairs(head: ListNode | null): ListNode | null {
+
+};
+// @lc code=end
+

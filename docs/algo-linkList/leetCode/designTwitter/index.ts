5d2020e4d14229f708d44dceaf78eebf3f58900b
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-08-16 10:55:38 +0800

    feat: update design twitter solutions

--- docs/algo-linkList/leetCode/designTwitter/index.ts
+++ docs/algo-linkList/leetCode/designTwitter/index.ts
@@ -6,23 +6,86 @@
 
 // @lc code=start
 
-class ListNode {
-  userId: number;
-  tweetId: number;
-  followerId: number;
-  followeeId: number;
+class User {
+  id: number;
+  followed: Set<number>;
+  head: Tweet | null;
+  constructor(useId: number) {
+    this.id = useId;
+    this.followed = new Set();
+    this.head = null;
+    this.follow(useId);
+  }
+  follow(userId: number) {
+    this.followed.add(userId);
+  }
+  unfollow(userId: number) {
+    this.followed.delete(userId);
+  }
+  post(tweetId: number) {
+    const timestamp = +new Date();
+    const tweet = new Tweet(tweetId, timestamp);
+    tweet.next = this.head;
+    this.head = tweet;
+  }
+}
+
+class Tweet {
+  tweetId?: number = -1;
+  tweetTime?: number = -1;
+  next: Tweet | null;
+  constructor(tweetId?: number, tweetTime?: number) {
+    this.tweetId = tweetId;
+    this.tweetTime = tweetTime;
+  }
 }
 
 export class Twitter {
-  constructor() {}
+  recentMax: number;
+  userMap: Map<number, User>;
+  constructor() {
+    this.recentMax = 10;
+    this.userMap = new Map();
+  }
 
-  postTweet(userId: number, tweetId: number): void {}
+  postTweet(userId: number, tweetId: number): void {
+    if (!this.userMap.has(userId)) {
+      this.userMap.set(userId, new User(userId));
+    }
+    const user = this.userMap.get(userId);
+    user.post(tweetId);
+  }
 
-  getNewsFeed(userId: number): number[] {}
+  getNewsFeed(userId: number): number[] {
+    const result: number[] = [];
+    if (!this.userMap.has(userId)) return result;
 
-  follow(followerId: number, followeeId: number): void {}
+    // userIds
+    const users = this.userMap.get(userId).followed;
 
-  unfollow(followerId: number, followeeId: number): void {}
+    // tweets
+    const tweets = [...users.values()].map(
+      (userId) => this.userMap.get(userId).head
+    );
+
+    // TODO: merge K sort list
+  }
+
+  follow(followerId: number, followeeId: number): void {
+    if (!this.userMap.has(followerId)) {
+      this.userMap.set(followerId, new User(followerId));
+    }
+    if (!this.userMap.has(followeeId)) {
+      this.userMap.set(followeeId, new User(followeeId));
+    }
+    this.userMap.get(followerId).follow(followeeId);
+  }
+
+  unfollow(followerId: number, followeeId: number): void {
+    if (this.userMap.has(followerId)) {
+      this.userMap.get(followerId).unfollow(followeeId);
+    }
+  }
 }
 
 /**

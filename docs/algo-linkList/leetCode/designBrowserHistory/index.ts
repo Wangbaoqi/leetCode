277967c4ee2298f6fa6bb13f4ae960326c916054
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-08-30 10:37:17 +0800

    feat: update design browser history

--- docs/algo-linkList/leetCode/designBrowserHistory/index.ts
+++ docs/algo-linkList/leetCode/designBrowserHistory/index.ts
@@ -1,3 +1,4 @@
+import { DoubleList } from './../LRUCache/index';
 /*
  * @lc app=leetcode.cn id=1472 lang=typescript
  *
@@ -5,14 +6,61 @@
  */
 
 // @lc code=start
-class BrowserHistory {
-  constructor(homepage: string) {}
 
-  visit(url: string): void {}
+class ListNode {
+  val: string;
+  next?: ListNode | null;
+  prev?: ListNode | null;
+  constructor(val: string) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
 
-  back(steps: number): string {}
+export class BrowserHistory {
+  currentPage: ListNode;
+  linkList: DoubleList;
+  constructor(homepage: string) {
+    const node = new ListNode(homepage);
+    this.currentPage = node;
+  }
 
-  forward(steps: number): string {}
+  visit(url: string): void {
+    const node = new ListNode(url);
+    node.next = null;
+    this.currentPage.next = node;
+    node.prev = this.currentPage;
+
+    this.currentPage = this.currentPage.next;
+  }
+
+  back(steps: number): string {
+    let node = this.currentPage;
+    while (steps) {
+      if (node.prev === null) {
+        break;
+      } else {
+        node = node.prev;
+      }
+      steps--;
+    }
+    this.currentPage = node;
+    return node.val;
+  }
+
+  forward(steps: number): string {
+    let node = this.currentPage;
+
+    while (steps) {
+      if (node.next === null) break;
+      node = node.next;
+      steps--;
+    }
+
+    this.currentPage = node;
+    return node.val;
+  }
 }
 
 /**

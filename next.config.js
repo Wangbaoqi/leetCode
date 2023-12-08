9a68f8597398477e4e68fcb67c1a3e72897ca10f
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-12-08 21:01:27 +0800

    fix: update config of build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-const { withContentlayer } = require('next-contentlayer')
+const withContentlayer = require('next-contentlayer').withContentlayer
 
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  // output: 'build',
+  distDir: 'build',
+  typescript: {
+    // ignoreBuildErrors: process.env.IS_VERCEL_ENV === "true",
+    ignoreBuildErrors: true,
+  },
+}
 
 module.exports = withContentlayer(nextConfig)

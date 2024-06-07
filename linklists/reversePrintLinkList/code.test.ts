f84644ae21c5546fff7874ec3bd041529731bcd6
Wangbaoqi <wangbaoqi8839@gmail.com>
2024-06-07 14:32:44 +0800

    feat: update file

--- linklists/reversePrintLinkList/code.test.ts
+++
@@ -1,9 +0,0 @@
-import { ListNode, reversePrint } from './code';
-
-describe('test reversePrintLinkList', () => {
-  it('test_reverse_print_link_list', () => {
-    const head = new ListNode(1, new ListNode(2, new ListNode(3)));
-    const expected = [3, 2, 1];
-    expect(reversePrint(head)).toEqual(expected);
-  });
-});
